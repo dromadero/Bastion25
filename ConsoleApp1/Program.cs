26bc2584496aaaff49f472bec9a4a596106e3326
dromadero <dev@dromadero.pl>
2025-04-27 19:59:06 +0200

    thwth4th

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Bastion.Common;
+
+Console.WriteLine("Hello, World!");
+
+AppLoger logger = new AppLoger("rolo", new HttpClient(), "http://192.168.1.225:8090");
+await logger.Log("tak  wkoncu sie udało :)))))!!!!!! jou jou ou");
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
